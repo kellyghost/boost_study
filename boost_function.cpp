7abfc845a05760e5bf28f4b77c56799f823212e8
your_name <your_email>
2018-11-12 16:18:40 +0800

    当boost::function对象为0d 时候，调用会出现boost::bad_function_call

--- boost_function.cpp
+++ boost_function.cpp
@@ -5,9 +5,20 @@
 
 int main()
 {
-  boost::function<int (const char*)> f = std::atoi;
-  std::cout  << f("1609") << std::endl;
+  // boost::function<int (const char*)> f = std::atoi;
+  // std::cout  << f("1609") << std::endl;
+  //
+  // f = std::strlen;
+  // std::cout  << f("1609") << std::endl;
 
-  f = std::strlen;
-  std::cout  << f("1609") << std::endl;
+
+  try
+  {
+    boost::function<int (const char*)> f ;
+    f("");
+  }
+  catch (boost::bad_function_call &ex)
+  {
+    std::cout << ex.what() << std::endl;
+  }
 }
