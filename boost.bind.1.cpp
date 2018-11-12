dc62b5da854d5f84d21fc29ac33a12ac25f671ce
your_name <your_email>
2018-11-12 15:42:25 +0800

    boost::bind 二元函数传递给std::sort

--- boost.bind.1.cpp
+++ boost.bind.1.cpp
@@ -1,20 +1,31 @@
-#include <boost/bind.hpp>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <boost/bind.hpp>
 
-void add(int i, int j)
+bool compare(int i, int j)
 {
-  std::cout << i+j << std::endl;
+  return i > j;
+
 }
 
+void print(int i)
+{
+   std::cout << i << std::endl;
+}
 
 int main()
 {
   std::vector<int> v;
   v.push_back(1);
-  v.push_back(2);
   v.push_back(3);
+  v.push_back(2);
 
-  std::for_each(v.begin(),v.end(),boost::bind(add,10,_1));
+
+  std::cout << "before" << std::endl;
+  std::sort(v.begin(),v.end(),boost::bind(compare,_1,_2));
+
+  std::cout << "after:" << std::endl;
+  std::for_each(v.begin(),v.end(),print);
+
 }
