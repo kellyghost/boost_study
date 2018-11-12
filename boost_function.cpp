ef9be930d2c0746d87d8c726c05403e1488a8b0f
your_name <your_email>
2018-11-12 17:29:07 +0800

    boost::function 对应的函数只有一个参数

--- boost_function.cpp
+++ boost_function.cpp
@@ -3,6 +3,17 @@
 #include <cstdlib>
 #include <cstring>
 
+
+
+struct world
+{
+  void hello(std::ostream &os)
+  {
+    os << "Hello,world!" << std::endl;
+  }
+};
+
+
 int main()
 {
   // boost::function<int (const char*)> f = std::atoi;
@@ -12,13 +23,18 @@ int main()
   // std::cout  << f("1609") << std::endl;
 
 
-  try
-  {
-    boost::function<int (const char*)> f ;
-    f("");
-  }
-  catch (boost::bad_function_call &ex)
-  {
-    std::cout << ex.what() << std::endl;
-  }
+  // try
+  // {
+  //   boost::function<int (const char*)> f ;
+  //   f("");
+  // }
+  // catch (boost::bad_function_call &ex)
+  // {
+  //   std::cout << ex.what() << std::endl;
+  // }
+
+  boost::function<void (world*,std::ostream&)> f = &world::hello;
+  world w;
+  f(&w,boost::ref(std::cout));
+
 }
